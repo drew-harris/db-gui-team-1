6081274f9a594ee3a963f190de39bd54c28ee8eb
Drew Harris <dsharris10@gmail.com>
2022-10-27 18:07:28 -0500

    Save jwt on login / register

--- frontend/src/api/movies.ts
+++ frontend/src/api/movies.ts
@@ -1,9 +1,16 @@
+import { getJwt } from "../utils/jwt";
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default async function getMovies({ page }) {
   try {
     const response = await fetch(
-      API_URL + "/api/movies?" + new URLSearchParams({ page: page })
+      API_URL + "/api/movies?" + new URLSearchParams({ page: page }),
+      {
+        headers: {
+          jwt: getJwt(),
+        },
+      }
     );
     if (!response.ok) {
       console.log(response);
