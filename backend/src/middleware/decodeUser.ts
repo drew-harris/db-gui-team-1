a7cb274ec00277a6c10a74e380c2c4f145da6c39
DukeBartholomew <78329199+DukeBartholomew@users.noreply.github.com>
2022-10-26 20:45:30 -0500

    Rating post

--- backend/src/middleware/decodeUser.ts
+++ backend/src/middleware/decodeUser.ts
@@ -2,6 +2,7 @@ import { verifyJWT } from "../utils/jwt.util";
 import { Request, Response, NextFunction } from "express";
 
 export default function decodeUser(req, res: Response, next: NextFunction) {
+  console.log(req);
   const accessToken: string = req.headers.jwt;
 
   const validToken = verifyJWT(accessToken);
