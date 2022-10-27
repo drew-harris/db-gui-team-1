b8c3436bc7d1cb0958bb5229596c3cbf23ee2e2d
Drew Harris <dsharris10@gmail.com>
2022-10-27 18:10:43 -0500

    Add super simple pagination

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import getMovies from "../api/movies";
 import SimpleMovie from "../components/SimpleMovie";
 import "../index.css";
 
 function Home() {
-  const [page, setPage] = useState(8);
+  const [page, setPage] = useState(1);
+  const client = useQueryClient();
   const {
     data: movies,
     status,
     error,
-  } = useQuery(["movies"], () => getMovies({ page: page }), {
+  } = useQuery(["movies", { page: page }], () => getMovies({ page: page }), {
     retry: false,
   });
+
+  useEffect(() => {
+    client.prefetchQuery(["movies", { page: page + 1 }], () =>
+      getMovies({ page: page + 1 })
+    );
+  }, [page]);
 
   return (
     <div className=" text-white px-5">
@@ -20,6 +27,7 @@ function Home() {
       {error && error instanceof Error && (
         <div>{error.message || "Error getting movies"} TEST</div>
       )}
+      <button onClick={() => setPage((curr) => curr + 1)}>Next page</button>
       <div className="flex flex-wrap gap-4 justify-around">
         {movies &&
           movies.map((movie) => {
