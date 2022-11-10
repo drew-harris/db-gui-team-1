958c944f640240678aa20c9f527d639a519f027c
Drew Harris <harrisd@smu.edu>
2022-11-10 16:08:19 -0600

    Remove unnecessary console.log

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,17 +1,9 @@
-import { Card, Group, Image, Rating, Text } from "@mantine/core";
-import { Movie } from "@prisma/client";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Card, Group, Image, Text } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 import { MovieWithCounts } from "../types";
 
-function randomRatingNumber() {
-  return Math.round(Math.random() * 5);
-}
-
 export default function MovieCard({ movie }: { movie: MovieWithCounts }) {
-  const [randomRating] = useState(randomRatingNumber());
   const navigate = useNavigate();
-  console.log(movie);
   return (
     <Card onClick={() => navigate("/movie/" + movie.id)}>
       <Card.Section>
