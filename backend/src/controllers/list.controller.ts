3edf986d29a11e7bba9e7c73fd9706efcd8b71cb
DukeBartholomew <78329199+DukeBartholomew@users.noreply.github.com>
2022-11-10 19:51:44 -0600

    "Create List Complete"

--- backend/src/controllers/list.controller.ts
+++ backend/src/controllers/list.controller.ts
@@ -1,21 +1,18 @@
 import { Response } from "express";
 import prisma from "../utils/prisma.util";
-import {
-    createList,
-} from "../services/list.services";
-
+import { createList } from "../services/list.service";
 
 export async function createListHandler(req, res: Response) {
-    try {
-      const list = await createList(req.body, req.user);
-      return res.json(list);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        error: {
-          error: error.message,
-          message: "Could not create new list",
-        },
-      });
-    }
+  try {
+    const list = await createList(req.body, req.user);
+    return res.json(list);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: {
+        error: error.message,
+        message: "Could not create new list",
+      },
+    });
   }
+}
