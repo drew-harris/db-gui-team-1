664396db9051ee61fabc531d7acb412e1bdb0c3e
Drew Harris <harrisd@smu.edu>
2022-11-14 11:13:46 -0600

    Log out

--- frontend/src/pages/profile/EditProfile.tsx
+++ frontend/src/pages/profile/EditProfile.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@mantine/core";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
 export const EditProfilePage = () => {
+  const { setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const signOut = () => {
+    window.localStorage.setItem("jwt", null);
+    window.localStorage.setItem("user", null);
+    setUser(null);
+    navigate("/");
+  };
   return (
     <div>
       <div>Edit profile</div>
+      <Button onClick={signOut}>Sign Out</Button>
     </div>
   );
 };
