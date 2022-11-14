d501cef129ffea7215e7acd9d8578595068d08b1
Drew Harris <harrisd@smu.edu>
2022-11-14 11:47:38 -0600

    Add ratings chip for user page

--- frontend/src/components/ratings/RatingChip.tsx
+++ frontend/src/components/ratings/RatingChip.tsx
@@ -1,5 +1,17 @@
-import { Box } from "@mantine/core";
+import { Box, Group, Paper, Rating, Text } from "@mantine/core";
+import { Link } from "react-router-dom";
 
-export const RatingChip = () => {
-  return <Box>Rating chip</Box>;
+export const RatingChip = ({ rating, showUser = false }) => {
+  return (
+    <Paper p="sm">
+      <Group>
+        {showUser && (
+          <Text component={Link} to={"/profile/" + rating.by.id}>
+            {rating.by.username}
+          </Text>
+        )}
+        <Rating value={rating.score} size="xs" readOnly></Rating>
+      </Group>
+    </Paper>
+  );
 };
--- frontend/src/pages/movies/MovieRatingsPage.tsx
+++ frontend/src/pages/movies/MovieRatingsPage.tsx
@@ -1,7 +1,39 @@
+import { Box, Title } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getMovieById } from "../../api/movies";
+import { getAllRatingsByMovieId } from "../../api/ratings";
+import { RatingChip } from "../../components/ratings/RatingChip";
+
 export const MovieRatingsPage = () => {
+  const { id } = useParams();
+
+  const { data: ratings } = useQuery(["ratings", { movieId: id }], () =>
+    getAllRatingsByMovieId(id)
+  );
+
+  const { data: movie } = useQuery(["movie", { id: id }], () =>
+    getMovieById(id)
+  );
+
+  if (!movie || !ratings) {
+    return null;
+  }
+
   return (
     <div>
-      <div>Movie ratings page</div>
+      <Title>Ratings For: {movie.title}</Title>
+      <Box
+        sx={(theme) => ({
+          display: "flex",
+          flexWrap: "wrap",
+          gap: theme.spacing.sm,
+        })}
+      >
+        {ratings.map((rating) => {
+          return <RatingChip showUser key={rating.id} rating={rating} />;
+        })}
+      </Box>
     </div>
   );
 };
