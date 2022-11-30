19cfba5b1a2b2a9de0d11e173bc88449127d15b4
Drew Harris <harrisd@smu.edu>
2022-11-29 18:25:26 -0600

    Show avatar

--- frontend/src/components/ratings/RatingChip.tsx
+++ frontend/src/components/ratings/RatingChip.tsx
@@ -1,4 +1,4 @@
-import { Group, Paper, Rating, Text } from "@mantine/core";
+import { Avatar, Group, Paper, Rating, Text } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 export const RatingChip = ({ rating, showUser = false }) => {
@@ -6,9 +6,12 @@ export const RatingChip = ({ rating, showUser = false }) => {
     <Paper p="sm" shadow="xs">
       <Group>
         {showUser && (
-          <Text component={Link} to={"/profile/" + rating.by.id}>
-            {rating.by.username}
-          </Text>
+          <Group>
+            <Avatar radius="xl" src={rating.by.profileImageUrl} />
+            <Text component={Link} to={"/profile/" + rating.by.id}>
+              {rating.by.username}
+            </Text>
+          </Group>
         )}
         <Rating value={rating.score} size="xs" readOnly></Rating>
       </Group>
